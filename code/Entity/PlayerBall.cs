518c2e5dfde37e5785bc42063f4c0f3c05400f14
Matt Stevens <matt@mattstevens.co.uk>
2021-04-28 16:49:38 +0100

    Better collision debug on PlayerBall.OnPhysicsCollision

--- code/Entity/PlayerBall.cs
+++ code/Entity/PlayerBall.cs
@@ -62,13 +62,14 @@ namespace Minigolf
 		/// <param name="timeDelta"></param>
 		protected override void OnPhysicsCollision(CollisionEventData eventData)
 		{
-			// We only want to adjust how we bounce off walls for now
-			// if (!eventData.Entity.IsWorld)
-			// 	return;
+			// Walls are non world cause it's fucked
+			if (eventData.Entity.IsWorld)
+				return;
 
-			Sandbox.UI.ChatBox.AddInformation(Player.All, $"Bounce at normal: {eventData.Normal}");
+			DebugOverlay.Text(eventData.Pos, $"Up Dot Normal: {Vector3.Up.Dot(eventData.Normal)}", Color.White, 5);
 
-			if (true || Vector3.Up.Dot(eventData.Normal) >= -0.35)
+			// Don't do ridiculous bounces upwards, just bounce off walls mainly
+			if (Vector3.Up.Dot(eventData.Normal) >= -0.35)
             {
 				// calculate our bounce normal
 				var normal = eventData.PreVelocity.Normal;
@@ -78,8 +79,8 @@ namespace Minigolf
 				var reflect = Vector3.Reflect(eventData.PreVelocity.Normal, eventData.Normal.Normal).Normal;
 				var newSpeed = Math.Max(eventData.PreVelocity.Length, eventData.Speed);
 
-				Sandbox.UI.ChatBox.AddInformation(Player.All, $"Reflect normal: {reflect}");
-				DebugOverlay.Line(eventData.Pos, eventData.Pos + (reflect * 32.0f), 5);
+				DebugOverlay.Line(eventData.Pos, eventData.Pos - (eventData.PreVelocity.Normal * 64.0f), 5);
+				DebugOverlay.Line(eventData.Pos, eventData.Pos + (reflect * 64.0f), 5);
 
 				Velocity = reflect * newSpeed * 0.8f;
 
