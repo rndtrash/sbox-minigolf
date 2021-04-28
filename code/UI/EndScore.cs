d3a129631ad4718e3984f8a81d54201aa23374a6
Matt Stevens <matt@mattstevens.co.uk>
2021-04-28 16:48:13 +0100

    UI testing for end scores

---
+++ code/UI/EndScore.cs
@@ -0,0 +1,45 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Threading.Tasks;
+
+namespace Minigolf
+{
+	public partial class EndScore : Panel
+	{
+		public static EndScore Current;
+
+		public bool Show;
+
+		private Label score;
+		private Label hole;
+
+		public EndScore()
+		{
+			Current = this;
+
+			StyleSheet.Load("/ui/EndScore.scss");
+
+			score = Add.Label("BOGEY", "score");
+			hole = Add.Label("HOLE 1", "hole");
+		}
+
+		public override void Tick()
+		{
+			Show = Player.Local.Input.Down(InputButton.Jump);
+
+			if (!Show)
+            {
+				(GolfHUD.Current as GolfHUD).Fade = false;
+				RemoveClass("show");
+				return;
+            }
+
+			AddClass("show");
+			// when we are showing, make sure the rootpanel is faded, probably a better way to do this
+			(GolfHUD.Current as GolfHUD).Fade = true;
+		}
+	}
+
+}
--- code/UI/GolfHUD.cs
+++ code/UI/GolfHUD.cs
@@ -1,17 +1,34 @@
 ﻿using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 namespace Minigolf
 {
 	[Library]
 	public partial class GolfHUD : Hud
 	{
+		private bool _fade;
+		public bool Fade {
+			get { return _fade; }
+			set {
+				if (value)
+					RootPanel.AddClass("fade");
+				else
+					RootPanel.RemoveClass("fade");
+
+				_fade = value;
+			}
+		}
+
 		public GolfHUD()
 		{
 			if (!IsClient) return;
 
+			RootPanel.StyleSheet.Load("/ui/GolfHUD.scss");
+
 			RootPanel.AddChild<Sandbox.UI.ChatBox>();
 			RootPanel.AddChild<PowerBar>();
+			RootPanel.AddChild<EndScore>();
 		}
 	}
 }
