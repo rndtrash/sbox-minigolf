494b8136659be236589523f4f5a3fb9f0a8ad9c6
Matt Stevens <matt@mattstevens.co.uk>
2021-04-29 03:29:49 +0100

    Add minigolf_hole_info for defining courses in a map alone.

---
+++ code/Entity/HoleInfo.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+
+namespace Minigolf
+{
+	[Library("minigolf_hole_info")]
+	public partial class HoleInfoEntity : ModelEntity
+	{
+		[HammerProp("hole")]
+		public int Hole { get; set; }
+		[HammerProp("name")]
+		public string Name { get; set; }
+		[HammerProp("par")]
+		public int Par { get; set; }
+
+
+		public override void Spawn()
+		{
+			base.Spawn();
+		}
+	}
+}
