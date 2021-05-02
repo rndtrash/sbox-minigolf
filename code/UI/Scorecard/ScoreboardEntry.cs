f059fff7d3eeb0d61f98cab70f14edfabf6f1e5c
Matt Stevens <matt@mattstevens.co.uk>
2021-05-02 21:22:56 +0100

    Add tacky scoreboard/card

---
+++ code/UI/Scorecard/ScoreboardEntry.cs
@@ -0,0 +1,34 @@
+﻿
+using Sandbox;
+using Sandbox.Hooks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+
+namespace Minigolf
+{
+	public partial class ScoreboardEntry : Panel
+	{
+		public PlayerScore.Entry Entry;
+
+		public Label PlayerName;
+
+		public ScoreboardEntry()
+		{
+			PlayerName = Add.Label( "PlayerName", "name" );
+			var panel = Add.Panel();
+			for (int i = 1; i < 9; i++)
+				panel.Add.Label("0");
+		}
+
+		public virtual void UpdateFrom( PlayerScore.Entry entry )
+		{
+			Entry = entry;
+
+			PlayerName.Text = entry.GetString( "name" );
+
+			SetClass( "me", Player.Local != null && entry.Get<ulong>( "steamid", 0 ) == Player.Local.SteamId );
+		}
+	}
+}
---
+++ code/UI/Scorecard/Scorecard.cs
@@ -0,0 +1,92 @@
+﻿using Sandbox;
+using Sandbox.Hooks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+
+namespace Minigolf
+{
+	public partial class Scorecard : Panel
+	{
+		public Panel Canvas { get; protected set; }
+		public TextEntry Input { get; protected set; }
+
+		Dictionary<int, ScoreboardEntry> Entries = new();
+
+		public Panel Header { get; protected set; }
+
+		public Scorecard()
+		{
+			StyleSheet.Load("/UI/Scorecard/Scorecard.scss");
+
+			var container = Add.Panel("container");
+
+			AddHeader(container);
+
+			Canvas = container.Add.Panel("canvas");
+
+			PlayerScore.OnPlayerAdded += AddPlayer;
+			PlayerScore.OnPlayerUpdated += UpdatePlayer;
+			PlayerScore.OnPlayerRemoved += RemovePlayer;
+
+			foreach (var player in PlayerScore.All)
+			{
+				AddPlayer(player);
+			}
+		}
+		public override void Tick()
+		{
+			base.Tick();
+
+			SetClass("open", Player.Local?.Input.Down(InputButton.Score) ?? false);
+		}
+
+
+		protected virtual void AddHeader(Panel container)
+		{
+			container.Add.Label("SCORECARD", "header");
+
+			{
+				var row = container.Add.Panel("row");
+				row.Add.Label("HOLE");
+				var holes = row.Add.Panel();
+				for (int i = 1; i < 9; i++)
+					holes.Add.Label(i.ToString());
+			}
+
+			{
+				var row = container.Add.Panel("row");
+				row.Add.Label("PAR");
+				var holes = row.Add.Panel();
+				for (int i = 1; i < 9; i++)
+					holes.Add.Label("3");
+			}
+		}
+
+		protected virtual void AddPlayer(PlayerScore.Entry entry)
+		{
+			var p = Canvas.AddChild<ScoreboardEntry>();
+			p.UpdateFrom(entry);
+
+			Entries[entry.Id] = p;
+		}
+
+		protected virtual void UpdatePlayer(PlayerScore.Entry entry)
+		{
+			if (Entries.TryGetValue(entry.Id, out var panel))
+			{
+				panel.UpdateFrom(entry);
+			}
+		}
+
+		protected virtual void RemovePlayer(PlayerScore.Entry entry)
+		{
+			if (Entries.TryGetValue(entry.Id, out var panel))
+			{
+				panel.Delete();
+				Entries.Remove(entry.Id);
+			}
+		}
+	}
+}
