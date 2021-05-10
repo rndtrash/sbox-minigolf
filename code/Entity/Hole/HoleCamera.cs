28ee2d85261838221d89280e1df8bebab067ec0f
Matt Stevens <matt@mattstevens.co.uk>
2021-05-10 19:10:11 +0100

    Add camera entities for each hole

---
+++ code/Entity/Hole/HoleCamera.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+
+namespace Minigolf
+{
+	[Library( "minigolf_hole_camera" )]
+	public partial class HoleCamera : ModelEntity
+	{
+		[HammerProp( "hole_number" )]
+		public int Hole { get; set; }
+		[HammerProp( "FOV" )]
+		public float FOV { get; set; }
+		[HammerProp( "ZNear" )]
+		public float ZNear { get; set; }
+		[HammerProp( "ZFar" )]
+		public float ZFar { get; set; }
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			Log.Info( $"Hole {Hole} Camera at [{WorldPos}] [{WorldAng}]" );
+
+			Transmit = TransmitType.Never;
+		}
+	}
+}
