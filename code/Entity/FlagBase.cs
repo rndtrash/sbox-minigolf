3a28dbd7ca79cfc295749a9d53790629a43fe6ed
Matt Stevens <matt@mattstevens.co.uk>
2021-05-11 04:05:45 +0100

    Add golf flag entity

---
+++ code/Entity/FlagBase.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minigolf
+{
+	[Library( "minigolf_flag_base" )]
+	public partial class FlagBase : ModelEntity
+	{
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			SetModel( "models/golf_flag.vmdl" );
+
+			MoveType = MoveType.None;
+			CollisionGroup = CollisionGroup.Never;
+			PhysicsEnabled = false;
+			UsePhysicsCollision = false;
+
+			Transmit = TransmitType.Always;
+		}
+	}
+}
