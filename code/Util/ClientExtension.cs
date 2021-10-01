f14c714f3e346ddd780b0daed522398a412cdba0
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:00:04 +0100

    Some Client extensions and proper SteamID struct

---
+++ code/Util/ClientExtension.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace Minigolf
+{
+	public static partial class ClientExtensions
+	{
+		public static Entity GetEntity( this Client self ) => Entity.FindByIndex( self.NetworkIdent );
+		public static SteamID SteamID( this Client self ) => new( self.SteamId );
+		public static bool IsBot( this Client self ) => self.SteamID().AccountType == SteamIDAccountType.AnonGameServer;
+		public static bool IsHost( this Client self ) => Global.IsListenServer && self.NetworkIdent == 1;
+	}
+}
