f5d6c811125769a6d0133efc877ec89df3172561
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:03:24 +0100

    Make a Camera component and use this for camera entities

--- code/Entity/Map/Attributes/FrustumAttribute.cs
+++ code/Entity/Map/Attributes/FrustumAttribute.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Sandbox;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Hammer
 {
-	class FrustumAttribute
+	[AttributeUsage( AttributeTargets.Class )]
+	public class FrustumAttribute : MetaDataAttribute
 	{
+		internal string FOVField;
+		internal string NearField;
+		internal string FarField;
+
+		public FrustumAttribute( string fovField, string nearField, string farField )
+		{
+			FOVField = fovField;
+			NearField = nearField;
+			FarField = farField;
+		}
+
+		public override void AddHeader( StringBuilder sb )
+		{
+			sb.Append( $" frustum( {FOVField}, {NearField}, {FarField} )" );
+		}
 	}
 }
