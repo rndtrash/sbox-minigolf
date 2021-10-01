9714aeb010a5521937de633d44afc119bd8967fa
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:27:35 +0100

    Remove complicated RPC synced Course class with a BaseNetworkable derived

--- code/Course.cs
+++ code/Course.cs
@@ -4,51 +4,58 @@ using System.Linq;
 
 namespace Minigolf
 {
-	/// <summary>
-	/// Custom network synced class
-	/// </summary>
-    public partial class Course
+    public partial class Course : BaseNetworkable
 	{
         public string Name { get; set; } = "Default";
-        public string Description { get; set; } = "Default Description";
+		public string Description { get; set; } = "Default Description";
 		public Dictionary<int, HoleInfo> Holes { get; set; }
 
-		private int _currentHole { get; set; } = 1;
-		public HoleInfo CurrentHole => Holes[_currentHole];
+		[Net] private int _currentHole { get; set; } = 1;
+		public HoleInfo CurrentHole
+		{
+			get
+			{
+				// Sanity check for networked
+				if ( Holes == null || !Holes.ContainsKey(_currentHole) )
+					return new HoleInfo { Number = _currentHole, Name = "oops" };
 
-		/// <summary>
-		/// Load the course info from the current map.
-		/// </summary>
-		public void LoadFromMap()
-        {
-            Host.AssertServer();
+				return Holes[_currentHole];
+			}
+		}
 
-			// Todo: Load Name / Description in
+		// Register for events
+		public Course() => Event.Register( this );
+		~Course() => Event.Unregister( this );
 
+		[Event.Entity.PostSpawn]
+		public void Load()
+		{
+			// TODO: Instead of loading the clientside this class could be a INetworkSerializable?
 			Holes = new Dictionary<int, HoleInfo>();
 
-            foreach (var hole in Entity.All.OfType<BallSpawnpoint>().OrderBy(ent => ent.HoleNumber))
-            {
-                var goal = Entity.All.OfType<HoleGoal>().Where(x => x.HoleNumber == hole.HoleNumber).First();
+			foreach ( var hole in Entity.All.OfType<BallSpawnpoint>().OrderBy( ent => ent.HoleNumber ) )
+			{
+				var goal = Entity.All.OfType<HoleGoal>().Where( x => x.HoleNumber == hole.HoleNumber ).First();
 
-                if (goal == null)
-                {
-                    Log.Error($"No ball goal found for [Hole {hole.HoleNumber}]");
-                    continue;
-                }
+				if ( goal == null )
+				{
+					Log.Error( $"No ball goal found for [Hole {hole.HoleNumber}]" );
+					continue;
+				}
 
-				Holes.Add(hole.HoleNumber, new HoleInfo()
-                {
-                    Number = hole.HoleNumber,
-                    Name = hole.HoleName,
-                    Par = hole.HolePar,
-                    SpawnPosition = hole.Position,
-                    SpawnAngles = hole.WorldAng,
-                });
-            }
-        }
+				Holes.Add( hole.HoleNumber, new HoleInfo()
+					{
+						Number = hole.HoleNumber,
+						Name = hole.HoleName,
+						Par = hole.HolePar,
+						SpawnPosition = hole.Position,
+						SpawnAngles = hole.Rotation.Angles(),
+						GoalPosition = goal.Position,
+					}
+				);
+			}
+		}
 
-		// TODO: SHIT CODE
         public void NextHole()
         {
 			var matchedHoles = Holes.Where( x => x.Key > _currentHole )
@@ -64,43 +71,11 @@ namespace Minigolf
 			_currentHole = nextHole.Key;
 
 			// Announce to all clients that the course has advanced.
-			clientNextHole( To.Everyone, _currentHole );
+			// clientNextHole( To.Everyone, _currentHole );
 
 			// Run an event so we can pick this up anywhere in the code base.
 			Event.Run( "minigolf.advanced_hole", _currentHole );
         }
-
-		[ClientRpc]
-		public static void clientNextHole( int newHole )
-		{
-			var course = GolfGame.Instance.Course;
-			if ( course == null )
-			{
-				Log.Error( "Tried to advance hole with no Course instance." );
-				return;
-			}
-
-			course._currentHole = newHole;
-			Event.Run( "minigolf.advanced_hole", course._currentHole );
-		}
-
-		[ServerCmd( "minigolf_debug_print_sv" )]
-		static void PrintCourse()
-		{
-			var game = Game.Current as GolfGame;
-			Log.Info( $"Coruse: {game.Course}, {game.Course.CurrentHole}, {game.Course.Holes}" );
-			Log.Info( $"{game.Course.Holes.Count} holes" );
-			foreach ( var hole in game.Course.Holes.Values )
-			{
-				Log.Info( $"\t[{hole.Number}] name = {hole.Name} par = {hole.Par}" );
-			}
-		}
-
-		[ClientCmd( "minigolf_debug_print_cl" )]
-		static void PrintCourseCl()
-		{
-			PrintCourse();
-		}
 	}
 
 	public struct HoleInfo
@@ -110,5 +85,6 @@ namespace Minigolf
 		public int Par;
 		public Vector3 SpawnPosition;
 		public Angles SpawnAngles;
+		public Vector3 GoalPosition;
     }
 }
