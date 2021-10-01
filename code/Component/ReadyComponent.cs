f2e419ecafdae5d7f245a54acde12c51086054f1
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:12:18 +0100

    Handle game state in a quick and dirty way using components

---
+++ code/Component/ReadyComponent.cs
@@ -0,0 +1,9 @@
+﻿using Sandbox;
+
+namespace Minigolf
+{
+	public partial class ReadyComponent : EntityComponent
+	{
+		[Net] public bool Ready { get; set; }
+	}
+}
--- code/Game.State.cs
+++ code/Game.State.cs
@@ -1,17 +1,80 @@
 ﻿using Sandbox;
+using System.Collections.Generic;
 
 namespace Minigolf
 {
-	partial class GolfGame
+	public enum GameState
 	{
-		public enum GameState
+		WaitingForPlayers,
+		Playing,
+		EndOfGame
+	}
+
+	partial class Game
+	{
+		[Net, Change( nameof( OnStateChanged ) )]
+		public GameState State { get; set; } = GameState.WaitingForPlayers;
+
+		[Net] public List<Client> PlayingClients { get; set; }
+
+		public void OnStateChanged( GameState oldState, GameState newState )
 		{
-			WaitingForPlayers,
-			Playing,
-			EndOfGame
+			Event.Run( "minigolf.state.changed", newState );
+			// Pass to HUD?
 		}
 
-		// [Net] public bool WaitingToStart { get; set; } = true;
-		// [Net] public float StartTime { get; set; }
+		[ServerCmd]
+		public static void Ready()
+		{
+			var client = ConsoleSystem.Caller;
+			if ( client == null ) return;
+
+			var readyComponent = client.Components.GetOrCreate<ReadyComponent>();
+			readyComponent.Ready = !readyComponent.Ready;
+
+			// TODO: move this to a tick or some shit
+			foreach ( var cl in Client.All )
+			{
+				var rdy = cl.Components.GetOrCreate<ReadyComponent>();
+				if ( !rdy.Ready ) return;
+			}
+
+			Current.StartGame();
+		}
+
+		public void StartGame()
+		{
+			PlayingClients = new List<Client>();
+			foreach( var client in Client.All )
+			{
+				var readyComponent = client.Components.GetOrCreate<ReadyComponent>();
+				if ( readyComponent.Ready )
+					PlayingClients.Add( client );
+			}
+
+			// Spawn balls for all clients
+			foreach ( var cl in PlayingClients )
+			{
+				cl.Components.Create<ScoreComponent>();
+
+				var ball = new Ball();
+				ball.ResetPosition( Course.CurrentHole.SpawnPosition, Course.CurrentHole.SpawnAngles );
+				cl.Pawn = ball;
+			}
+
+			State = GameState.Playing;
+		}
+
+		[AdminCmd( "minigolf_force_start" )]
+		public static void ForceStart()
+		{
+			// Force everyone to ready
+			foreach ( var client in Client.All )
+			{
+				client.Components.GetOrCreate<ReadyComponent>().Ready = true;
+			}
+
+			Current.StartGame();
+		}
 	}
 }
