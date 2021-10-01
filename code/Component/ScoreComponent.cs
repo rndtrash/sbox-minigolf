1a1ca658ea86da122e3322715aec8ca4663924f7
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:23:56 +0100

    Add a Score component with a INetworkSerializer to keep on Client's for scoring

---
+++ code/Component/ScoreComponent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sandbox;
+
+namespace Minigolf
+{
+	public partial class ScoreComponent : EntityComponent, INetworkSerializer
+	{
+		public int Score
+		{
+			get
+			{
+				if ( Scores.ContainsKey( Game.Current.Course.CurrentHole.Number ) )
+					return Scores[Game.Current.Course.CurrentHole.Number];
+				return 0;
+			}
+			set
+			{
+				Scores[Game.Current.Course.CurrentHole.Number] = value;
+				WriteNetworkData();
+			}
+		}
+		public Dictionary<int, int> Scores { get; set; } = new();
+		public int Total => Scores.Values.Sum();
+
+		void INetworkSerializer.Write( NetWrite write )
+		{
+			write.Write( Scores.Count );
+			foreach ( var score in Scores )
+			{
+				write.Write( score.Key );
+				write.Write( score.Value );
+			}
+
+			Log.Info( $"{this} Wrote {Scores.Count}" );
+		}
+
+		void INetworkSerializer.Read( NetRead read )
+		{
+			Scores = new();
+
+			var count = read.Read<int>();
+			for ( int i = 0; i < count; i++ )
+			{
+				var key = read.Read<int>();
+				var value = read.Read<int>();
+
+				Scores[key] = value;
+			}
+
+			Log.Info( $"{this} Read {count}" );
+		}
+	}
+}
