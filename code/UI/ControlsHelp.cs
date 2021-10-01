264ec122b18d06680a1c3d60f34c463e93920dc1
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:25:19 +0100

    Placeholder for control helpers

--- code/UI/ControlsHelp.cs
+++ code/UI/ControlsHelp.cs
@@ -12,12 +12,16 @@ namespace Minigolf
 		{
 			StyleSheet.Load( "/ui/ControlsHelp.scss" );
 
+			AddEntry( "keyboard", "Other Bullshit" );
 			AddEntry( "mouse", "Move Camera" );
+			AddEntry( "keyboard", Input.GetKeyWithBinding( "+iv_score" ) );			
 		}
 
 		protected void AddEntry(string icon, string name)
 		{
-
+			var row = Add.Panel( "row" );
+			row.Add.Icon( icon, "icon" );
+			row.Add.Label( name, "text" );
 		}
 	}
 
