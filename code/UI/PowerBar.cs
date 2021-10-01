0a9ce872a6d2c9915e58d0d325fab0d477551ce2
Matt Stevens <matt@mattstevens.co.uk>
2021-10-01 03:17:28 +0100

    Return of the PowerBar

---
+++ code/UI/PowerBar.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Minigolf
+{
+	[UseTemplate]
+	public partial class PowerBar : Panel
+	{
+		Panel Bar { get; set; }
+
+		public override void Tick()
+		{
+			if ( Local.Pawn is not Ball ball ) return;
+
+			Bar.Style.Width = Length.Percent( ball.ShotPower * 100 );
+			Bar.Style.Dirty();
+		}
+	}
+
+}
